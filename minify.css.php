006b9c04f4c50ec636592db743e8a21abcf8a2f1
Kalaiselvan Veeramani <kalaiselvanveeramani@gmail.com>
2017-11-13 16:36:03 +0530

    Create minify.css.php

---
+++ minify.css.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-type: text/css');
+ob_start("compress");
+
+    function compress( $minify )
+    {
+	/* remove comments */
+    	$minify = preg_replace( '!/*[^*]**+([^/][^*]**+)*/!', '', $minify );
+
+        /* remove tabs, spaces, newlines, etc. */
+    	$minify = str_replace( array("rn", "r", "n", "t", '  ', '    ', '    '), '', $minify );
+
+        return $minify;
+    }
+
+    /* css files for combining */
+    include('reset.css');
+    include('application.css');
+    include('responsive.css');
+
+ob_end_flush();
+
+?>
